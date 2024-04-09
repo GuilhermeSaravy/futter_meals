ae88f266f7bf214bc5d3351dc161b95ba0218f9d
Guilherme Saravi <gsaravi@psgtecnologia.com.br>
2024-04-08 21:45:42 -0400

    [feat] Meal component item

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,31 @@
 import 'package:flutter/material.dart';
 
+import './screens/categories_screen.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'DeliMeals',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+            primary: Colors.pink,
+            secondary: Colors.amber,
+            background: const Color.fromRGBO(255, 254, 229, 1)),
+        // canvasColor: const ,
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              headline6: const TextStyle(
+                fontSize: 20,
+                fontFamily: 'RobotoCondensed',
+              ),
+            ),
       ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('DeliMeals'),
-      ),
-      body: Center(
-        child: Text('Navegar é preciso!!'),
-      ),
+      home: const CategoriesScreen(),
     );
   }
 }
--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -1,17 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:meals/components/category_item.dart';
+import '../data/dummy_data.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20,
-        ),
-        children: const [Text('data')]);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Vamos Cozinhar?'),
+        backgroundColor: Theme.of(context).primaryColor,
+      ),
+      body: GridView(
+          padding: const EdgeInsets.all(25),
+          gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20,
+          ),
+          children: DUMMY_CATEGORIES.map((cat) {
+            return CategoryItem(cat);
+          }).toList()),
+    );
   }
 }
