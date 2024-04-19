82cfd422cb3c2dc7aa904a0bf7d90fca460a7aa6
Guilherme Saravi <gsaravi@psgtecnologia.com.br>
2024-04-18 23:29:04 -0400

    Criando tela de configuracao

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:meals/screens/meal_detail_screen.dart';
+import 'package:meals/screens/settings_screen.dart';
 import 'package:meals/screens/tabs_screen.dart';
 
-import './screens/categories_screen.dart';
 import './screens/categories_meals_screen.dart';
 import './utils/app-routes.dart';
 
+import 'models/meal.dart';
+import 'data/dummy_data.dart';
+
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<Meal> _avaliableMeals = dummyMeals;
 
   @override
   Widget build(BuildContext context) {
@@ -30,8 +40,10 @@ class MyApp extends StatelessWidget {
       ),
       routes: {
         AppRoutes.HOME: (ctx) => TabsScreen(),
-        AppRoutes.CATEGORIES_MEALS: (ctx) => CategoriesMealsScreen(),
+        AppRoutes.CATEGORIES_MEALS: (ctx) =>
+            CategoriesMealsScreen(_avaliableMeals),
         AppRoutes.MEAL_DETAIL: (ctx) => MealDetailScreen(),
+        AppRoutes.SETTINGS: (ctx) => SettingsScreen(),
       },
     );
   }
